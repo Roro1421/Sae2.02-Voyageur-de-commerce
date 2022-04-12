196b0884bdaa03581d6d6a1353b5f848db0740ef
Kiemou <calibre1245@gmail.com>
2022-04-12 16:57:58 +0200

    Modification TypeAlgorithme

--- VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/TypeAlgorithme.cs
+++ VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/TypeAlgorithme.cs
@@ -4,6 +4,10 @@
     public enum TypeAlgorithme
     {
         ALGOEXEMPLE,
-        CROISSANT
+        CROISSANT,
+        PLUSPROCHEVOISIN,
+        INSERTIONPROCHE,
+        INSERTIONLOIN,
+        VOISINAGETOURNEE
     }
 }
