acf4a4f6cbad1e493ce1c1cc7db653e13c9c297e
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2022-04-22 23:23:20 +0200

    début recherche local

--- VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeVoisinageTournee.cs
+++ VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeVoisinageTournee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using VoyageurDeCommerce.modele.distances;
 using VoyageurDeCommerce.modele.lieux;
 
@@ -23,7 +18,53 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
         /// <param name="listeRoute">Liste de toutes les routes du graphe concerné</param>
         public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
         {
-            
+            FloydWarshall.calculerDistances(listeLieux, listeRoute);
+
+            int distanceTotalMin = Distance(listeLieux);
+
+            List<Lieu> voisinage = new List<Lieu>(listeLieux);
+            List<Lieu> meilleurVoisinage = new List<Lieu>(listeLieux);
+
+            for (int i = 0; i < listeLieux.Count; i++)
+            {
+                Swap(voisinage, i, (i + 1) % voisinage.Count);
+
+                int distanceTotal = Distance(voisinage);
+
+                if (distanceTotal < distanceTotalMin)
+                {
+                    distanceTotalMin = distanceTotal;
+                    meilleurVoisinage = voisinage;
+                }
+                voisinage = listeLieux;
+            }
+
+            foreach (Lieu l in meilleurVoisinage)
+            {
+                Tournee.Add(l);
+            }
+            this.NotifyPropertyChanged("Tournee");
+
         }
+
+        public void Swap(List<Lieu> listeLieu, int indexA, int indexB) 
+        {
+            Lieu tmp = listeLieu[indexA];
+            listeLieu[indexA] = listeLieu[indexB];
+            listeLieu[indexB] = tmp; 
+        }
+
+        public int Distance(List<Lieu> ListeLieux)
+        {
+
+                int resultat = 0;
+                for (int i = 0; i < ListeLieux.Count; i++)
+                {
+                    resultat += FloydWarshall.Distance(ListeLieux[i], ListeLieux[(i + 1) % ListeLieux.Count]);
+                }
+                return resultat;
+
+        }
+
     }
 }
