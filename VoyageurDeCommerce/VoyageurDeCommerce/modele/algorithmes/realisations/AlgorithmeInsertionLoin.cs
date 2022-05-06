7efe714c60b8f2f8dd4a89af8da6f0a9df158ab5
Kiemou <calibre1245@gmail.com>
2022-05-06 21:41:01 +0200

    Ajout classe abstraite AlgorithmeInsertion / Algos d'insertions modif héritage

--- VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeInsertionLoin.cs
+++ VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeInsertionLoin.cs
@@ -12,7 +12,7 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
     /// <summary>
     /// Gère l'algorithme d'insertion loin
     /// </summary>
-    public class AlgorithmeInsertionLoin : Algorithme
+    public class AlgorithmeInsertionLoin : AlgorithmeInsertion
     {
 
         public override string Nom => "Insertion loin";
@@ -91,44 +91,6 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
                 sw.Start();
             }
             this.TempsExecution = sw.ElapsedMilliseconds;
-        }
-
-        /// <summary>
-        /// Retourne la distance entre un lieu et un couple de lieux
-        /// </summary>
-        /// <param name="A">Lieu hors couple</param>
-        /// <param name="B">Premier lieu du couple</param>
-        /// <param name="C">Deuxième lieu du couple</param>
-        /// <returns>Entier de la distance entre le lieu et le couple de lieux</returns>
-        public int distanceCouple(Lieu A, Lieu B, Lieu C)
-        {
-            int distance = 0;
-
-            //Calcul de la distance du point A au couple de point B,C
-            distance = FloydWarshall.Distance(B, A) + FloydWarshall.Distance(A, C) - FloydWarshall.Distance(B, C);
-            return distance;
-        }
-
-        /// <summary>
-        /// Retourne la distance entre un lieu et une tournée
-        /// </summary>
-        /// <param name="T">Liste de Lieu représentant la tournée</param>
-        /// <param name="A">Lieu hors tournée</param>
-        /// <returns></returns>
-        public int distanceTournee(List<Lieu> T, Lieu A, out int positionInsertion)
-        {
-            int distance = distanceCouple(A, T[0], T[1]);
-            positionInsertion = 1;
-
-            for (int i = 0; i < T.Count; i++)
-            {
-                if (distanceCouple(A, T[i], T[(i + 1) % T.Count]) <= distance)
-                {
-                    distance = distanceCouple(A, T[i], T[(i + 1) % T.Count]);
-                    positionInsertion = i + 1;
-                }
-            }
-            return distance;
         }
     }
 }
