0998863abf8a2a233ef2ae59af91d5d0b9ee9d39
Romain BARABANT <rb416270@iut21.u-bourgogne.fr>
2022-05-18 10:02:19 +0200

    add random

--- VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRandom.cs
+++ VoyageurDeCommerce/VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRandom.cs
@@ -20,21 +20,60 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
 
             FloydWarshall.calculerDistances(listeLieux, listeRoute);
 
-            List<Lieu> lieuVisité = new List<Lieu>(listeLieux);
-            Random rand = new Random();
-            int index;
-            for (int i = 0; i < listeLieux.Count; i++)
-            {
-                index = rand.Next(0, lieuVisité.Count);
-                Tournee.Add(lieuVisité[index]);
-                lieuVisité.RemoveAt(index);
+            List<Lieu> tourne = new List<Lieu>();
+            tourne = repeateRandom(listeLieux,1000);
 
+            foreach(Lieu l in tourne)
+            {
+                Tournee.Add(l);
                 sw.Stop();
                 this.NotifyPropertyChanged("Tournee");
                 sw.Start();
-
             }
-            this.TempsExecution = sw.ElapsedMilliseconds;
+
+
+
+            //this.TempsExecution = sw.ElapsedMilliseconds;
+
+        }
+
+        public List<Lieu> random(List<Lieu> listeLieux, out int distance)
+        {
+            List<Lieu> lieuNonVisite = new List<Lieu>(listeLieux);
+            List<Lieu> lieuVisite = new List<Lieu>();
+            Random rand = new Random();
+            distance = 0;
+            int index;
+            for (int i = 0; i < listeLieux.Count; i++)
+            {
+                index = rand.Next(0, lieuNonVisite.Count);
+                lieuVisite.Add(lieuNonVisite[index]);
+                if (i != 0)
+                {
+                    distance += FloydWarshall.Distance(lieuVisite[lieuVisite.Count - 1], lieuVisite[lieuVisite.Count - 2]);
+                }
+                lieuNonVisite.Remove(lieuNonVisite[index]);
+            }
+            distance += FloydWarshall.Distance(lieuVisite[lieuVisite.Count - 1], lieuVisite[0]);
+            return lieuVisite;
+        }
+
+        public List<Lieu> repeateRandom(List<Lieu> listeLieux,int nbRepeate)
+        {
+            int distance;
+            int minDistance = 100000;
+            List<Lieu> tourne = new List<Lieu>();
+            List<Lieu> meilleurTourne = new List<Lieu>();
+            for (int i = 0; i < nbRepeate; i++)
+            {
+                tourne = random(listeLieux, out distance);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    meilleurTourne = tourne;
+                }
+            }
+            return meilleurTourne;
         }
     }
 }
